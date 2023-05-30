7e3d04b6bb827c47c9b6fafa849ed2cc1c2c8bec
Moheud Malic <mohidalamsohail22@live.com>
2023-05-30 23:30:57 +0300

    added all the security middleware

--- backend/app.js
+++ backend/app.js
@@ -16,10 +16,22 @@ const errorHandlerMiddleware = require("./middleware/errorhandler.js")
 const notFoundMiddleware = require("./middleware/notfoundmiddlware.js")
 // const {expressAsyncErrors} = require('express-async-errors')
 
+//extra security middleware
+const helmet = require("helmet")
+const cors = require("cors")
+const xss = require("xss-clean")
+const rateLimiter = require("express-rate-limit")
 
 const server = express()
 
+server.set('trust proxy', 1)
+server.use(rateLimiter({windowMs: 15*60*1000,  //15 minutes
+ max:100, }))  //max 100 requests allowed 
 server.use(express.json())
+server.use(helmet())
+server.use(cors())
+server.use(xss())
+
 
 server.use("/api/v1/notes", notesRoutes)
 server.use("/api/v1/auth", authRoutes)
